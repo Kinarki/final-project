5e0087923afa7cea6567326dec5670240c03a986
Kinarki <nickfriend81@gmail.com>
2015-12-14 18:33:55 -0800

    win test

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-var app = angular.module('tugowarApp', ['ngRoute']);
+var app = angular.module('tugowarApp', []);
+var counter = 0;
+$(document).on('keyup', function(e){
+    if(e.keyCode == 40){
+       counter += 1;
+        console.log(counter);
+    }
+    if(counter === 40){
+        alert('win');
+        counter = 0;
+    }
+});
